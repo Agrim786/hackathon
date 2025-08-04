e30f3ce30d678c85f3bb4519fdfdc9ffb7f1c68a
Agrim786 <agrimmahajam786@gmail.com>
2025-08-04 16:50:19 +0530

    Forecast

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Router as WouterRouter, Switch, Route } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,21 +8,18 @@ import About from "@/pages/about";
 import Contact from "@/pages/contact";
 import Privacy from "@/pages/privacy";
 import NotFound from "@/pages/not-found";
-import ScrollToTop from "./components/ScrollToTop";
 
-function Routes() {
+function Router() {
+  console.log("Backend URL:", import.meta.env.VITE_URL);
   return (
-    <>
-      <ScrollToTop /> 
-      <Switch>
-        <Route path="/" component={Dashboard} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/privacy" component={Privacy} />
-        <Route component={NotFound} />
-      </Switch>
-    </>
+    <Switch>
+      <Route path="/" component={Dashboard} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/privacy" component={Privacy} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
@@ -30,10 +27,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <WouterRouter>
-          <Toaster />
-          <Routes />
-        </WouterRouter>
+        <Toaster />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
